64c3c7ebb26a24677e4dc02fffb65cf6877cb710
alpana1project-pos <alpana1project@gmail.com>
2026-09-07 05:29:27 +0700

    chore: trigger production deploy for latest Alvin fixes

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import "./chat-history.css";
 
+// Alvin chat UI: new chat, history, delete, and provider diagnostics.
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><App /></React.StrictMode>
 );
